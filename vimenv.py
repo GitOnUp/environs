b435f2be975ab2ced5f8837ca8a62845a86be20a
George Murray <murray.george.m@gmail.com>
2014-08-08 14:13:10 -0700

    Fix file to find for molokai

--- vimenv.py
+++ vimenv.py
@@ -18,7 +18,7 @@ plugins = [
         VimPlugin('airline.vim', 'Airline', 'https://github.com/bling/vim-airline'),
         VimPlugin('fugitive.vim', 'Fugitive', 'git://github.com/tpope/vim-fugitive.git'),
         VimPlugin('vimproc.vim', 'vimproc', 'https://github.com/Shougo/vimproc.vim.git'),
-        VimPlugin('molokai', 'Molokai', 'https://github.com/tomasr/molokai.git'),
+        VimPlugin('molokai.vim', 'Molokai', 'https://github.com/tomasr/molokai.git'),
     ]
 
 _dotvim = path.expanduser('~/.vim')
